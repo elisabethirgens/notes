38f6f78af92bdbe1e20d7b6440846229c9b03057
Elisabeth Irgens <hello@elisabethirgens.com>
2020-12-18 22:46:35 +0100

    Add syntax highlighting 🎨

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("d LLL yyyy");
   });
+  eleventyConfig.addPlugin(syntaxHighlight);
   return {
     dir: {
       input: './',
